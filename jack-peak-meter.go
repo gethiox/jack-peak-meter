bbce85d9f4689f9e664389e0f194d23168e1e1bf
Gethiox <gethiox@gmail.com>
2017-10-21 23:04:49 +0200

    replace snake_case with camelCase aka converting Python into GoLang

--- jack-peak-meter.go
+++ jack-peak-meter.go
@@ -10,25 +10,25 @@ import (
 	"os/signal"
 )
 
-const channels = 2              // Amount of input channels
-const channel_buffer = 10        // Smoothing graph with last n printed samples, set 1 to disable
-const arbitrary_amplifier = 3.5 // Compensate weak audio signal with this ultimate amplifier value
+const channels = 2             // Amount of input channels
+const channelBuffer = 10       // Smoothing graph with last n printed samples, set 1 to disable
+const arbitraryAmplifier = 3.5 // Compensate weak audio signal with this ultimate amplifier value
 
 const disableCursor = "\033[?25l"
 const enableCursor = "\033[?25h"
 const moveCursorUp = "\033[F"
 
-var additional_buffer int
+var additionalBuffer int
 
-var buffer_size int
+var bufferSize int
 var PortsIn []*jack.Port
 var client *jack.Client
-var avg_main [channels]float32
+var avgMain [channels]float32
 var counter int
 var avg float32
 
-var print_title *bool
-var print_values *bool
+var printTitle *bool
+var printValues *bool
 
 func process(nframes uint32) int {
 	counter += 1
@@ -43,19 +43,19 @@ func process(nframes uint32) int {
 				avg = avg + float32(sample)
 			}
 		}
-		avg = avg * arbitrary_amplifier
-		avg = float32(avg) / float32(buffer_size)
+		avg = avg * arbitraryAmplifier
+		avg = float32(avg) / float32(bufferSize)
 		
-		avg_main[i] += avg
+		avgMain[i] += avg
 		
-		if counter >= additional_buffer {
-			printBar(avg_main[i]/float32(additional_buffer), i, int(getWidth()))
-			avg_main[i] = 0
+		if counter >= additionalBuffer {
+			printBar(avgMain[i]/float32(additionalBuffer), i, int(getWidth()))
+			avgMain[i] = 0
 		}
 		
 		fmt.Print("\n")
 	}
-	if counter >= additional_buffer {
+	if counter >= additionalBuffer {
 		counter = 0
 	}
 	for i := 0; i < channels; i++ {
@@ -70,16 +70,16 @@ func shutdown() {
 }
 
 func main() {
-	print_title = flag.Bool("title", false, "Print name of my ultimate visualizer")
-	print_values = flag.Bool("values", false, "Print value before each channel of visualizer")
+	printTitle = flag.Bool("title", false, "Print name of my ultimate visualizer")
+	printValues = flag.Bool("values", false, "Print value before each channel of visualizer")
 	flag.Parse()
 	
 	var status int
-	var client_name string
+	var clientName string
 	
 	for i := 0; i < 10; i++ {
-		client_name = fmt.Sprintf("spectrum analyser %d", i)
-		client, status = jack.ClientOpen(client_name, jack.NoStartServer)
+		clientName = fmt.Sprintf("spectrum analyser %d", i)
+		client, status = jack.ClientOpen(clientName, jack.NoStartServer)
 		if status == 0 {
 			break
 		}
@@ -92,8 +92,8 @@ func main() {
 	defer client.Close()
 	
 	fmt.Print(disableCursor)
-	buffer_size = int(client.GetBufferSize())
-	additional_buffer = calculate_additional_buffer(buffer_size)
+	bufferSize = int(client.GetBufferSize())
+	additionalBuffer = calculateAdditionalBuffer(bufferSize)
 	
 	if code := client.SetProcessCallback(process); code != 0 {
 		fmt.Println("Failed to set process callback:", code)
@@ -111,23 +111,23 @@ func main() {
 	for i := 0; i < channels; i++ {
 		port := client.PortRegister(fmt.Sprintf("input_%d", i), jack.DEFAULT_AUDIO_TYPE, jack.PortIsInput, 0)
 		if i%2 == 0 {
-			client.Connect("system:monitor_1", fmt.Sprintf("%s:input_%d", client_name, i))
+			client.Connect("system:monitor_1", fmt.Sprintf("%s:input_%d", clientName, i))
 		} else {
-			client.Connect("system:monitor_2", fmt.Sprintf("%s:input_%d", client_name, i))
+			client.Connect("system:monitor_2", fmt.Sprintf("%s:input_%d", clientName, i))
 		}
 		PortsIn = append(PortsIn, port)
 	}
 	
 	fmt.Print("\n\n")
-	terminal_widh := int(getWidth())
-	title_length := len(">>> ULTIMATE SOUND VISUALIZER 2,000,000 <<<")
-	if *print_title && terminal_widh >= title_length {
-		title_message := "%s>>> ULTIMATE SOUND VISUALIZER 2,000,000 <<<%s\n"
+	terminalWidh := int(getWidth())
+	titleLength := len(">>> ULTIMATE SOUND VISUALIZER 2,000,000 <<<")
+	if *printTitle && terminalWidh >= titleLength {
+		titleMessage := "%s>>> ULTIMATE SOUND VISUALIZER 2,000,000 <<<%s\n"
 		fill := ""
-		for i := 0; i < (terminal_widh-title_length)/2; i++ {
+		for i := 0; i < (terminalWidh-titleLength)/2; i++ {
 			fill += " "
 		}
-		fmt.Print(fmt.Sprintf(title_message, fill, fill))
+		fmt.Print(fmt.Sprintf(titleMessage, fill, fill))
 	}
 	
 	sigChan := make(chan os.Signal, 2)
@@ -141,42 +141,42 @@ func main() {
 	<-make(chan struct{})
 }
 
-var fill_h = []string{" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"}
-var last_values [channels][channel_buffer]float32
+var fill = []string{" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"}
+var lastValues [channels][channelBuffer]float32
 
-func update_cache(value float32, channel int) {
-	l := channel_buffer - 1
+func updateCache(value float32, channel int) {
+	l := channelBuffer - 1
 	for i := l; i > 0; i-- {
-		last_values[channel][i] = last_values[channel][i-1]
+		lastValues[channel][i] = lastValues[channel][i-1]
 	}
-	last_values[channel][0] = value
+	lastValues[channel][0] = value
 }
 
-func get_avg(channel int) float32 {
+func getAvg(channel int) float32 {
 	var avg float32
-	for _, v := range last_values[channel] {
+	for _, v := range lastValues[channel] {
 		avg += v
 	}
-	avg = avg / float32(channel_buffer)
+	avg = avg / float32(channelBuffer)
 	if avg > 1 {
 		avg = 1
 	}
 	return avg
 }
 
-func calculate_additional_buffer(frame_size int) int {
-	if frame_size > 512 {
+func calculateAdditionalBuffer(frameSize int) int {
+	if frameSize > 512 {
 		return 1
 	}
-	return int(512 / frame_size)
+	return int(512 / frameSize)
 }
 
 func printBar(value float32, channel int, width int) {
-	update_cache(value, channel)
-	value = get_avg(channel)
+	updateCache(value, channel)
+	value = getAvg(channel)
 	
 	var bar = ""
-	if *print_values {
+	if *printValues {
 		width -= 10
 		bar = fmt.Sprintf("\r %.3f |", value)
 	} else {
@@ -186,16 +186,16 @@ func printBar(value float32, channel int, width int) {
 	
 	chars := int(float32(width) * value)
 	for i := 0; i < chars; i++ {
-		bar += fill_h[8]
+		bar += fill[8]
 	}
 	
 	if chars < width {
-		fill_index := (float32(width)*value - float32(chars)) * 8
-		bar += fill_h[int(fill_index)]
+		fillIndex := (float32(width)*value - float32(chars)) * 8
+		bar += fill[int(fillIndex)]
 	}
 	
 	for i := 0; i <= width-chars-2; i++ {
-		bar += fill_h[0]
+		bar += fill[0]
 	}
 	
 	fmt.Print(bar + "| ")
